7a88bc6fd0f12eb08449f96fcf411df78cf9cb4c
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2018-05-30 15:31:59 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -34,19 +34,34 @@ alert("Sorry, try again");
 }
 
 //FlipCard Function 
-var flipCard = function (cardId) {
-console.log ("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-if (cardsInPlay.length === 2) {
-checkForMatch();
-}
+var flipCard = function () {
+	cardId = this.getAttribute('data-id');
+	console.log ("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+
 console.log (cards[cardId].cardImage);
 console.log (cards[cardId].suit);
-} 
+}; 
 
-flipCard(0);
-flipCard(2);
 
+//CreateBoard Function
+var createBoard = function (){
+	for (var i=0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}	
+
+}
+
+createBoard();
 
 
 
